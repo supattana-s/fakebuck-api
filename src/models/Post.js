9749e80bbac8ec8b0c310006773405e5ed3f104b
supattana-s <supattana.sue@gmail.com>
2022-12-14 15:24:13 +0700

    add models

---
+++ src/models/Post.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    const Post = sequelize.define(
+        "Post",
+        {
+            title: DataTypes.STRING,
+            image: DataTypes.STRING,
+        },
+        {
+            underscored: true,
+        }
+    );
+
+    return Post;
+};
